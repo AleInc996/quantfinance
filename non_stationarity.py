1c09e0ca63e8110631bc880bf6f695d89dc9d3c4
AleInc996 <71233145+AleInc996@users.noreply.github.com>
2024-05-01 23:04:35 +0200

    non_stationarity_v5

--- non_stationarity.py
+++ non_stationarity.py
@@ -24,6 +24,7 @@ from arch.unitroot import ZivotAndrews
 import yfinance as yf
 import yfinance.shared as shared
 import sys
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 # function for data retrieval
 def get_usable_data(ticker_choice, start_date, end_date):
@@ -163,8 +164,64 @@ if ziv_and.pvalue >= 0.05:
 else:
     print("The process is weakly stationary, due to the fact that the p-value ", ziv_and.pvalue, " is less than significance level 0.05")
 
+#------------------------------------------------------------------------------------------
 
 """
 But your question would be: why all of this? Why checking for stationarity or non-stationarity of a time series is so important?
+If a time series is non-stationary then it means that the first and/or the second moment of the distribution is dependent of time,
+the autocovariance changes over time and the time series does not tend to go back towards an average value.
+This means that with a non-stationary time series it is most likely to happen that we will observe trends or patterns,
+and these are difficult to capture in our time series modeling approach. For example, if we run an OLS to the time series,
+these special characteristics of the time series will not be described by our model (at least not formally).
+
+That is why we need to move from a non-stationary time series to a stationary one, which displays mean reversion and where
+the first and second moment of the distributions are not dependent of time. This will also help us in understanding the long-term features of this time series.
+Ultimately, our goal is to understand how trends and patterns in the time series evolve in the time interval we are analyzing.
+One common approach to make a time series stationary is to take the log returns first difference, in addition to study the rolling time series 
+and eventual trend and seasonality components. In case after the first difference we still have a non-stationary time series,
+the common approach would suggest to go ahead with analysis by taking the second difference and so on and so forth.
 
 """
+
+### Additional analysis on non-stationarity of the considered time series
+
+roll_mean_prices = stock_prices.rolling(window = 10).mean() # calculating rolling mean with a window of 10 days
+
+roll_std_prices = stock_prices.rolling(window = 10).std() # calculating rolling standard deviation with a window of 10 days
+
+plt.plot(roll_mean_prices, color = 'green', label = 'Rolling Mean of stock prices') # plotting the rolling mean
+
+plt.xlabel('Time') # adding labels
+plt.ylabel('Value')
+plt.title('Apple Rolling Statistics: Mean') # adding title
+
+plt.legend() # adding a legend
+plt.show()
+
+"""
+By plotting the rolling mean, with stocks, it is most likely to happen that we will observe a plot with a clear presence of a drift.
+The mean of the displayed time series will not revert to zero.
+
+What we would expect is that by taking the first difference of log returns we can get a stationary time series.
+But first, let's explore additional analysis on the prices time series.
+"""
+
+plt.plot(roll_std_prices, color = 'red', label = 'Rolling Standard deviation of stock prices') # plotting the rolling standard deviation
+
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.title('Apple Rolling Statistics: Standard Deviation')
+
+plt.legend()
+plt.show()
+
+"""
+Non-stationarity might also be suggested by the rolling standard deviation plot: if the magnitude of the rolling standard deviation changes over time,
+meaning that there are periods with a lower and periods with a higher magnitude, that could be a signal of non-stationarity of the time series.
+In the example of Apple, rolling standard deviation gets larger and larger as we move to recent days.
+"""
+
+
+
+### Replicating analysis with log returns
+
